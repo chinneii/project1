f97dad8a9e8c91f445f14f04d26e446ef6067d10
chinneii <chincy02@gmail.com>
2021-12-07 21:44:52 +0800

    timeChecker

--- js.js
+++ js.js
@@ -6,10 +6,35 @@ class Point {
         this._latitude = latitude;
 
     }
+    get name() {
+        return this._name;
+    }
+    get longitude() {
+        return this._longitude;
+    }
+    get latitude() {
+        return this._latitude;
+    }
+    set name(name) {
+        this._name = name;
+    }
+    set longitude(longitude) {
+        this._longitude = longitude;
+    }
+    set latitude(latitude) {
+        this._latitude = latitude;
+    }
+
+    fromData(data) {
+        this._name = data.name;
+        this._longitude = data.longitude;
+        this._latitude = data.latitude;
+    }
 }
 
 accuracy = 0.001;
 plannedLocations = [];
+
 
 
 var x = document.getElementById("spot");
@@ -22,25 +47,24 @@ function getLocation() {
     }
 }
 let interval = setInterval(getLocation, 5000);
-// function showPosition(position) {
-//     x.innerHTML = "Latitude: " + position.coords.latitude +
-//         "<br>Longitude: " + position.coords.longitude;
-// }
 
+function timeConverter(time) {
+
+}
 
 function checker(position) {
     let long = position.coords.longitude;
     let lat = position.coords.latitude;
-    console.log(long)
-    console.log(lat)
+
+    let time = new Date(position.timestamp)
+
+
     for (let i in plannedLocations) {
         let longCheck = Math.abs(long - plannedLocations[i].long);
         let latCheck = math.abs(lat - plannedLocations[i].lat);
         if (longCheck < accuracy && latCheck < accuracy) {
-            alert("user has checked into the destined location")
+            alert(`user has checked into ${plannedLocations[i].name} at ${time}`)
             plannedLocations.splice(i, 1)
-        } else {
-
         }
     }
 }

a570020bf63e1a3c437209c12bd28ffad81358bc
FLUORYYNX,_LIM <flim0012@student.monash.edu>
2021-12-08 16:02:31 +0800

    m

--- list.js
+++ list.js
@@ -4,13 +4,13 @@ savedLocation = retrieveLSData(SAVED_LOCATIONS_KEY);
 function pageLoad() {
 	let vacationList = document.getElementById("vacationList");
 	let vacationListInnerHTML = "";
-	for (let i in savedLocation) {
+	for (let i in savedLocation._trip) {
 		vacationListInnerHTML += `
 		<div class="mdl-cell mdl-cell--12-col mdl-cell--8-col-tablet mdl-cell--4-col-phone" >
 								<h5>Plan ${Number(i) + 1} <button onclick="detail(${i})" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">More information </button> </h5>
 								<div class="mdl-card">
 									<div class="mdl-card__supporting-text">
-										<b>Date:</b> ${savedLocation[i].date}
+										<b>Date:</b> ${savedLocation._trip[i].date}
 									</div>
 									<div class="mdl-card__actions mdl-card--border">
 									</div>

d88550cfdec01524f1fe6edbe936606f0c6c9263
FLUORYYNX,_LIM <flim0012@student.monash.edu>
2021-12-08 23:07:36 +0800

    .-.

--- detection.js
+++ detection.js
@@ -1,4 +1,4 @@
-accuracy = 0.01;
+accuracy = 10;
 let plannedLocations = [];
 let data;
 
@@ -21,9 +21,11 @@ function checker(position) {
         let longCheck = Math.abs(long - savedLocation._trip[0][i]._longitude);
         let latCheck = Math.abs(lat - savedLocation._trip[0][i]._latitude);
         console.log(longCheck)
+        checkedIn=document.getElementById(`checkin${i}`)
         if (longCheck < accuracy && latCheck < accuracy) {
             //change here for the id shit and stuff
-            alert(`user has checked into ${savedLocation._trip[0][i].name} at ${time}`)
+            //alert(`user has checked into ${savedLocation._trip[0][i].name} at ${time}`)
+        checkedIn.innerHTML=`user has checked into ${savedLocation._trip[0][i]._name} at ${time}`
             savedLocation._trip.splice(i, 1)
         }
     }

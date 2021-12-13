8c0b0cfb440fa59d8cc0af58c27178159565dad9
FLUORYYNX,_LIM <flim0012@student.monash.edu>
2021-12-13 23:09:46 +0800

    .

--- detection.js
+++ detection.js
@@ -7,15 +7,24 @@ retrieveLSData(LISTKEY)
 function getLocation() {
     navigator.geolocation.getCurrentPosition(checker);
 }
+
 let interval = setInterval(getLocation, 5000);
 
+function showAddress(data) {
+    let startAddress = data.results[0].formatted;
+    currentAddress= startAddress
+}
+
+let currentAddress="";
+
 function checker(position) {
+    console.log(position);
     let long = position.coords.longitude;
     let lat = position.coords.latitude;
+    sendWebServiceRequestForReverseGeocoding(lat, long, 'showAddress')
     console.log(long)
     console.log(lat)
     let time = new Date(position.timestamp)
-
 
     for (let i = 0; i < savedLocation._trip[0].length; i++) {
         let longCheck = Math.abs(long - savedLocation._trip[0][i]._longitude);
